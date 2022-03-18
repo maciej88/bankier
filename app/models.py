f0741278bd3536ff65a2d0b3abf93653014fe9db
maciej <mkrywult88@gmail.com>
2022-03-18 23:17:41 +0100

    models still not compatible

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class Stock(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=64, verbose_name='Name')
     code = models.CharField(max_length=32, verbose_name='Code')
     price = models.DecimalField(decimal_places=4, max_digits=4, verbose_name='Price')
