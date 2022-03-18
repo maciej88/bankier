17d71652a5ac1a8c7fde6629c4397c5ad796cfa5
maciej <mkrywult88@gmail.com>
2022-03-18 17:33:49 +0100

    model and migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=64, verbose_name='Name')
+    code = models.CharField(max_length=32, verbose_name='Code')
+    price = models.IntegerField(verbose_name='Price')
+    date =  models.DateTimeField(verbose_name='Date')
