ea70fae1099cc8d9ea358dfd63faec1979d5866d
maciej <mkrywult88@gmail.com>
2022-03-18 22:43:57 +0100

    filtering

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView
 from app.models import Stock
+from .filters import StockFilter
+
 
 class BankerListView(ListView):
     model = Stock
-    queryset = Stock.objects.all()
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = StockFilter(self.request.GET, queryset=self.get_queryset())
+        return context
