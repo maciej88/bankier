7d4e104bdc1d396bae782f3b13f90d9af1fd93dd
maciej <mkrywult88@gmail.com>
2022-03-18 19:37:42 +0100

    code format

--- data_collecting.py
+++ data_collecting.py
@@ -7,7 +7,8 @@ import sqlite3
 def get_data():
     db = sqlite3.connect('db.sqlite3')
     cursor = db.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Stock (name varchar(64), code varchar(32), price double precision , date TIMESTAMP)''')
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS Stock (name varchar(64), code varchar(32), price double precision , date TIMESTAMP)''')
     link = 'https://www.bankier.pl/gielda/notowania/akcje'
     now = dt.now()
     page = get(link)
@@ -15,14 +16,17 @@ def get_data():
     for actions in bs.find_all('tr'):
         try:
             code = str(actions.a.string)
-            price = actions.find('td', class_='colKurs change down').get_text() or actions.find('td', class_='colKurs change up').get_text() or actions.find('td', class_='colKurs change ').get_text()
+            price = actions.find('td', class_='colKurs change down').get_text() or actions.find('td',
+                                                                                                class_='colKurs change up').get_text() or actions.find(
+                'td', class_='colKurs change ').get_text()
             for td in actions.find_all('td', class_='colWalor textNowrap'):
                 name = td.a['title']
-                cursor.execute('INSERT INTO Stock VALUES (?, ?, ?, ?)', (name, code, price, now))
+                cursor.execute('INSERT OR REPLACE INTO Stock VALUES (?, ?, ?, ?)', (name, code, price, now))
                 db.commit()
 
         except AttributeError:
             continue
     db.close()
+
 
 get_data()
